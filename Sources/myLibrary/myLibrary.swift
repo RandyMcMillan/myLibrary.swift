b4cf01c81703ac3bef1b786786d24188825b0599
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-04-10 12:11:28 -0400

    myLibrary.swift: more detect and print

--- Sources/myLibrary/myLibrary.swift
+++ Sources/myLibrary/myLibrary.swift
@@ -1,32 +1,29 @@
 #if os(Linux)
+// Code specific to Linux
 #if swift(>=5.3.2)
     public private(set) var swift_version = "Running Swift 5.3.2 or later on(Linux)"
 #else
-    public private(set) var swift_version = "Running Swift 5.3.2 or earlier on(Linux)"
+    public private(set) var swift_version = "Running Swift earlier than 5.3.2 on(Linux)"
 #endif
-// Code specific to Linux
+// End Code specific to Linux
 
-//
 #elseif os(macOS)
+// Code specific to macOS
 #if swift(>=5.3.2)
     public private(set) var swift_version = "Running Swift 5.3.2 or later on(macOS)"
 #else
-    public private(set) var swift_version = "Running Swift 5.3.2 or earlier on(macOS)"
+    public private(set) var swift_version = "Running Swift earlier than 5.3.2 on(macOS)"
 #endif
-// Code specific to macOS
-
-//
+// End Code specific to macOS
 #endif
 
 #if canImport(UIKit)
 #if swift(>=5.3.2)
     public private(set) var swift_version = "Running Swift 5.3.2 or later with canImport(UIKit)"
 #else
-    public private(set) var swift_version = "Running Swift 5.3.2 or earlier with canImport(UIKit)"
+    public private(set) var swift_version = "Running Swift earlier than 5.3.2 with canImport(UIKit)"
 #endif
 // Code specific to platforms where UIKit is available
-
-//
 #endif
 
 public struct myLibrary {
